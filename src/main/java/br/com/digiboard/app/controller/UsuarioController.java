a393db8517d3907b084779a456230d73cb722b6a
Felipe Vasques <57740418+vasques-io@users.noreply.github.com>
2023-03-28 07:53:59 -0400

    Adicionado método de pesquisa
    Troca de anotação @SessionScope para @ViewScoped - necessário para renderizar os conteúdo atual da view;
    Método de pesquisar por nomes adicionado

--- src/main/java/br/com/digiboard/app/controller/UsuarioController.java
+++ src/main/java/br/com/digiboard/app/controller/UsuarioController.java
@@ -2,32 +2,27 @@ package br.com.digiboard.app.controller;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.faces.view.ViewScoped;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.RequestScope;
-import org.springframework.web.context.annotation.SessionScope;
 
 import br.com.digiboard.app.model.Usuario;
 
 import br.com.digiboard.app.repository.UsuarioRepository;
 
 @Component
-@SessionScope
+@ViewScoped
 public class UsuarioController {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
-	@PersistenceContext
-	private EntityManager entityManager;
+	private String searchText;
 	
 	private Usuario usuario = new Usuario();
 	
@@ -36,6 +31,10 @@ public class UsuarioController {
 	@PostConstruct
 	public void init() {
 		usuarios = usuarioRepository.findAll();
+	}
+	
+	public void procurar() {
+		usuarios = usuarioRepository.findByNomeContaining(searchText);
 	}
 	
 	public void salvar() {
@@ -54,8 +53,8 @@ public class UsuarioController {
 	}
 	
 	public void excluir(Usuario usuario) {
+		usuarios.remove(usuario);
 		usuarioRepository.delete(usuario);
-		usuarios = usuarioRepository.findAll();
 	}
 
 	public Usuario getUsuario() {
@@ -73,6 +72,15 @@ public class UsuarioController {
 	public void setUsuarios(List<Usuario> usuarios) {
 		this.usuarios = usuarios;
 	}
+
+	public String getSearchText() {
+		return searchText;
+	}
+
+	public void setSearchText(String searchText) {
+		this.searchText = searchText;
+	}
+	
 	
 
 }
