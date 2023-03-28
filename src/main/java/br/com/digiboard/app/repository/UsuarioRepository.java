b1bee3b9bfacae6e0843e87536e9185440e9c93b
Felipe Vasques <57740418+vasques-io@users.noreply.github.com>
2023-03-28 07:50:32 -0400

    Método para retornar lista de nomes

--- src/main/java/br/com/digiboard/app/repository/UsuarioRepository.java
+++ src/main/java/br/com/digiboard/app/repository/UsuarioRepository.java
@@ -1,11 +1,8 @@
 package br.com.digiboard.app.repository;
 
-import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.digiboard.app.model.Usuario;
@@ -13,6 +10,6 @@ import br.com.digiboard.app.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-	
+	List<Usuario> findByNomeContaining(String nome);
 	
 }
